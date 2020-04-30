5b09edaa3ff39b943a49b8af5579029ad24105aa
massinissanm <massinissa.naitmouloud@gmail.com>
2020-04-30 15:29:51 +0200

    ajout structure

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,16 +9,50 @@ var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lil
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 ////////////////////
 
+// Variable de test
+var notLogged = false;
 
-/* GET search page. */
-router.get('/search', async function(req, res, next) {
 
-  var journey = await journeyModel.find();
+/* GET home page. */
+router.get('/', function(req, res, next) {
 
-  res.render('search', { title: 'Express' });
+  res.render('index');
 });
 
+/* GET Search. */
+router.get('/search', async function(req, res, next) {
+  if(!req.session.user) {
+    notLogged = true;
+    res.redirect('/');
+  }
+  res.render('search');
+});
 
+/* Search POST traitement avec date. */
+router.post('/search-trajet', async function(req, res, next) {
+  req.session.trajets = await journeyModel.findOne({departure:req.body.depart} && {arrival:req.body.arrivee} && {date:req.body.date});
+  console.log(trajets);
+  res.redirect('/result', {trajets:req.session.trajets});
+});
+
+/* GET Results. */
+router.get('/result', function(req, res, next) {
+  //initialisation du tableau des tickets
+  req.session.tickets = [];
+  res.render('result', {trajets:req.session.trajets});
+});
+
+/* GET My Tickets Pages. */
+router.get('/my-tickets', async function(req, res, next) {
+
+  res.render('tickets', {tickets:req.session.tickets});
+});
+
+/* GET My Trips Pages. */
+router.get('/my-trips', async function(req, res, next) {
+
+  res.render('trips', {trips});
+});
 
 
 module.exports = router;
