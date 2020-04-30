5370c32259674be454f6398d230a8f3aaa755cab
Andrew Pruvost <avpruvost@gmail.com>
2020-04-30 12:32:57 +0200

    Ajout de deux routes POST pour Sign-in et Sign-up

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,57 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST formulaire Sign-up */
+
+router.post('/sign-up', async function(req,res,next){
+
+  var searchUser = await userModel.findOne({
+    email: req.body.emailFromFront
+  })
+  
+  if(!searchUser){
+    var newUser = new userModel({
+      username: req.body.usernameFromFront,
+      email: req.body.emailFromFront,
+      password: req.body.passwordFromFront,
+    })
+  
+    var newUserSave = await newUser.save();
+  
+    req.session.user = {
+      name: newUserSave.username,
+      id: newUserSave._id,
+    }
+  
+    console.log(req.session.user)
+  
+    res.redirect('/')
+  } else {
+    res.redirect('/')
+  }
+  
+})
+
+/* POST formulaire Sign-in */
+
+router.post('/sign-in', async function(req,res,next){
+
+  var searchUser = await userModel.findOne({
+    email: req.body.emailFromFront,
+    password: req.body.passwordFromFront
+  })
+
+  if(searchUser!= null){
+    req.session.user = {
+      name: searchUser.username,
+      id: searchUser._id
+    }
+    res.redirect('/')
+  } else {
+    res.render('')
+  }
+
+  
+})
+
 module.exports = router;
